a80af93c6d04c9430de97392ee0657c3a655589c
Caden Friedenbach <cfrieden@redhat.com>
2017-03-07 22:40:52 -0500

    Finished HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
 import { AppComponent } from './app.component';
+import { HeroSearchService } from './hero-search.service';
+import { HeroSearchComponent } from './hero-search.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -17,15 +22,18 @@ import { AppRoutingModule } from './app-routing.module';
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
-  providers: [ HeroService ],
+  providers: [ HeroService,
+    HeroSearchService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
